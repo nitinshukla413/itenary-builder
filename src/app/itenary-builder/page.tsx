e294a7f5780993964c625f8ed8af25ce6b2ae808
Priyanka Raghuvanshi <priyanka.raghuvanshi@bajajfinserv.in>
2024-03-17 19:40:05 +0530

    add

--- src/app/itenary-builder/page.tsx
+++ src/app/itenary-builder/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Header from '@/components/header'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Travel = () => {
   const [noOfDays, setNoOfDays] = useState<any[]>([0, 1, 2, 3, 4, 5]);
@@ -17,13 +17,13 @@ const Travel = () => {
     <div className="bg-white h-[100vh] w-[100vw] flex justify-center  item-center">
       <div className="w-[80%] h-full p-10">
         <div className="flex space-x-30 ">
-          <div className="flex rounded-3xl w-full overflow-x-scroll bg-gray-200 justify-between items-center">
+          <div className="flex rounded-3xl w-full overflow-x-scroll scrollbar-hide no-scrollbar bg-gray-200 justify-between items-center">
             {noOfDays?.map((no: any) => {
               let value = `Day ${no}`
               if (no === 0) {
                 value = `Cover`
               }
-              return <div onClick={() => handleSelect(no)} className={`bg-gray-200 p-3 cursor-pointer w-full rounded-3xl flex justify-center items-center ${selectedDay === no ? 'bg-blue-400' : ''}`}>
+              return <div onClick={() => handleSelect(no)} className={`bg-gray-200 p-3 cursor-pointer min-w-60 rounded-3xl flex justify-center items-center ${selectedDay === no ? 'bg-blue-400' : ''}`}>
                 <h3 className={`text-black text-center text-[17px] font-[400] ${selectedDay === no ? 'text-white' : ''}`}>{value}</h3>
               </div>
             })
